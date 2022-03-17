c7c038ddee706c289a77e10749b143cc76f7dd9a
masuhi <masoomahasnainpunjwani@gmail.com>
2022-03-16 17:50:26 -0700

    Finally solved it!g

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <unistd.h>
+
 /**
  * main - prints _putchar
  *
@@ -7,16 +7,11 @@
  *
  * Return: 0 (Success)
  */
-int _putchar(char c)
-
-{
-	return (write(1, &c, 1));
-
-}
 
 int main(void)
 {
 	char text[9] = "_putchar";
+	
 	int i = 0;
 
 	for(i=0; i < 8; i++)
