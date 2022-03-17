eb88c6d446d2dfb65396c5ddf118cf0ea87a5ccf
masuhi <masoomahasnainpunjwani@gmail.com>
2022-03-16 18:02:14 -0700

    Trying to clear up bety style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,13 @@
  */
 int main(void)
 {
-	char text[9] = "_putchar";
-	
+	char text[9] = "_putchar"
 	int i = 0;
 
-	for(i=0; i < 8; i++)
+	for (i = 0; i < 8; i++)
 	{
 		_putchar(text[i]);
 	}
 	_putchar('\n');
-	return 0;
+	return (0);
 }
