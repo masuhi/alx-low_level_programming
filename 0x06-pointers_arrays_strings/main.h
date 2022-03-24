c6f34a2199dcf956598b56a6edc3250a1d48cf3c
masuhi <99360684+masuhi@users.noreply.github.com>
2022-03-25 00:04:25 +0300

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,31 +1,30 @@
-#ifndef FILE_MAIN
+#include "main.h"
 
-#define FILE_MAIN
+/**
+ * _strcat - concatenates two strings
+ * @dest: string to append to
+ * @src: string to add
+ *
+ * Return: a pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
 
+	i = 0;
+	j = 0;
 
+	while (dest[i] != '\0')
+		i++;
 
-int _putchar(char c);
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
 
-char *_strcat(char *dest, char *src);
+	dest[i] = '\0';
 
-char *_strncat(char *dest, char *src, int n);
-
-char *_strncpy(char *dest, char *src, int n);
-
-int _strcmp(char *s1, char *s2);
-
-void reverse_array(int *a, int n);
-
-char *string_toupper(char *);
-
-char *cap_string(char *);
-
-char *leet(char *);
-
-char *rot13(char *);
-
-void print_number(int n);
-
-
-
-#endif
+	return (dest);
+}
